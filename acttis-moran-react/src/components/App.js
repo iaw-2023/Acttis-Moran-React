30f819ef5d716da8c46c7df33856d4f2125b13c7
LeanM <leandro.moran.11@gmail.com>
2023-05-19 11:43:50 -0300

    Setup the routing schema of the App
    Established the routing schema between pages on App, using dummie pages

--- acttis-moran-react/src/components/App.js
+++ acttis-moran-react/src/components/App.js
@@ -1,7 +1,23 @@
 import "../css/App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Layout from "./pagewrappers/Layout";
+import Contact from "./Contact";
+import Missing from "./Missing";
 
 function App() {
-  return <div>Landing page</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Catch all (pages that doesnt exists) */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
