9657bf5db8924b0c3d253341259391d0125a3e63
LeanM <leandro.moran.11@gmail.com>
2023-05-19 18:30:17 -0300

    Page wrapper with nav bar and footer

--- acttis-moran-react/src/index.js
+++ acttis-moran-react/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
