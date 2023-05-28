2f625b866ce06273fe55055200a7436302cad322
LeanM <42581931+LeanM@users.noreply.github.com>
2023-05-28 16:09:30 -0300

    Styling navbar and back button

--- src/components/pagewrappers/PageWrapper.js
+++ src/components/pagewrappers/PageWrapper.js
@@ -6,18 +6,52 @@ import { useContext, useEffect, useState } from "react";
 import CartContext from "../../context/CartProvider";
 
 export default function PageWrapper(props) {
-
   const { cart, setCart } = useContext(CartContext);
   const [cartInfoItems, setCartInfoItems] = useState(cart.length);
+  const [navStyle, setNavStyle] = useState({});
+
+  const rootStyle = document.querySelector(":root");
+  const cssVariables = getComputedStyle(rootStyle);
 
   useEffect(() => {
     setCartInfoItems(cart.length);
-    }, [cart]);
+  }, [cart]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
+  const listenToScroll = () => {
+    if (window.pageYOffset === 0) {
+      setNavStyle(styleNavBarTransparent);
+    } else {
+      setNavStyle(styleNavBarSolid);
+    }
+  };
+
+  const handleMouseOver = () => {
+    setNavStyle(styleNavBarSolid);
+  };
+
+  const styleNavBarTransparent = {
+    backgroundColor: cssVariables.getPropertyValue("--nav-color-transparent"),
+    height: "4.5rem",
+  };
+
+  const styleNavBarSolid = {
+    backgroundColor: cssVariables.getPropertyValue("--nav-color"),
+  };
 
   return (
     <div className="page__container">
       <div id="page__background"></div>
-      <section className="page__wrapper__section" id="home__wrapper__section">
+      <section
+        style={navStyle}
+        className="page__wrapper__section"
+        id="home__wrapper__section"
+        onMouseOver={handleMouseOver}
+      >
         <nav className="page__wrapper__navbar">
           <div className="page__wrapper__navbar__container">
             <div className="page__wrapper__navbar__enterpriseName">
