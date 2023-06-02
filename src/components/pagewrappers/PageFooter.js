e4ef9282386b387d0910dec6c64ab0975dfb3905
LeanM <42581931+LeanM@users.noreply.github.com>
2023-06-02 01:22:26 -0300

    Footer finished

--- src/components/pagewrappers/PageFooter.js
+++ src/components/pagewrappers/PageFooter.js
@@ -1,84 +1,67 @@
 import "../../css/footer.css";
-import { MDBFooter, MDBBtn, MDBIcon, MDBContainer } from "mdb-react-ui-kit";
 
 export default function PageFooter() {
   return (
-    <MDBFooter className="footer-container text-center">
-      <MDBContainer className="p-4 pb-0">
-        <section className="mb-4">
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="facebook-f" />
-          </MDBBtn>
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p className="text-justify">
+              Welcome to "You Ticket" – your one-stop destination for purchasing
+              football tickets to exhilarating European team matches! Whether
+              you're a die-hard fan or a casual observer, "You Ticket" offers a
+              seamless platform to secure your spot in the electrifying
+              atmosphere of top-tier football stadiums across Europe.
+            </p>
+          </div>
 
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="twitter" />
-          </MDBBtn>
-
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="google" />
-          </MDBBtn>
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="instagram" />
-          </MDBBtn>
-
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="linkedin-in" />
-          </MDBBtn>
-
-          <MDBBtn
-            outline
-            color="light"
-            floating
-            className="m-1"
-            href="#!"
-            role="button"
-          >
-            <MDBIcon fab icon="github" />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
-
-      <div className="text-center p-3">
-        © 2023 Copyright:
-        <a className="text-white" href="#">
-          You Ticket
-        </a>
+          <div className="col-xs-6 col-md-3">
+            <h6>References</h6>
+            <ul className="footer-links">
+              <li>
+                <a href="https://www.framer.com/motion/">Framer Motion</a>
+              </li>
+              <li>
+                <a href="https://react-hot-toast.com/">React Hot Toast</a>
+              </li>
+              <li>
+                <a href="https://mdbootstrap.com/docs/react/">
+                  MDB React Bootstrap
+                </a>
+              </li>
+              <li>
+                <a href="https://fonts.google.com/">Google Fonts</a>
+              </li>
+              <li>
+                <a href="https://fontawesome.com/">Font Awesome</a>
+              </li>
+              <li>
+                <a href="https://react-select.com/home">React Select</a>
+              </li>
+              <li>
+                <a href="https://www.npmjs.com/package/react-text-transition">
+                  React Text Transition
+                </a>
+              </li>
+              <li>
+                <a href="https://react-bootstrap.github.io/">React Bootstrap</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr />
       </div>
-    </MDBFooter>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-6 col-xs-12">
+            <p className="copyright-text">
+              Copyright &copy; 2023 All Rights Reserved by
+              <a href="#"> You Ticket</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
