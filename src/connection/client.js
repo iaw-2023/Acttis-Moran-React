0abe3cd9d181643d12552634de724c3e3bc98c63
LeanM <42581931+LeanM@users.noreply.github.com>
2023-08-15 14:34:23 -0300

    Using env variable for server path

--- src/connection/client.js
+++ src/connection/client.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+import React from "react";
 
-const server_path = "http://localhost:8000/restapi";
-const vercel_path = "https://best-ai-prompts-backend.vercel.app/restapi";
+//const server_path = "http://localhost:8000/restapi";
+//const vercel_path = "https://best-ai-prompts-backend.vercel.app/restapi";
+
+const server_path = process.env.REACT_APP_SERVER_PATH;
 
 export default axios.create({
   baseURL: server_path,
