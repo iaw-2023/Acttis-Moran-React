8c4811f82962ac5368f4c33871bc43db9904e3be
LeanM <42581931+LeanM@users.noreply.github.com>
2023-08-15 13:02:47 -0300

    Update client.js

--- src/connection/client.js
+++ src/connection/client.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const server_path = "http://localhost:8000/restapi";
+const vercel_path = "https://best-ai-prompts-backend.vercel.app";
 
 export default axios.create({
-  baseURL: server_path,
+  baseURL: vercel_path,
   headers: { "Content-Type": "application/json" },
 });
 
