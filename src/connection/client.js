7793f17b2d4afcf035fca183279400cfed3320e9
LeanM <42581931+LeanM@users.noreply.github.com>
2023-11-29 12:13:05 -0300

    Env

--- src/connection/client.js
+++ src/connection/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const server_path = "https://acttis-moran-laravel-sage.vercel.app/restapi/";
+const server_path = process.env.REACT_APP_SERVER_PATH;
 
 export default axios.create({
   baseURL: server_path,
